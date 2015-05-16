9d23563b6eef7cf45cb1ec346dfc7ceaceced57d
liruqi <liruqi@gmail.com>
2015-05-16 20:45:12 +0800

    修改个人资料 UI 完善

--- DoubleDate/Class/Settings/DDPersonalUpdateController.m
+++ DoubleDate/Class/Settings/DDPersonalUpdateController.m
@@ -15,24 +15,18 @@
 #import "NewSettingViewController.h"
 #import "DDDataManager.h"
 
+#define  PIC_WIDTH 80
+#define  PIC_HEIGHT 80
 
 @interface DDPersonalUpdateController () <UIImagePickerControllerDelegate>
 {
-    
+    NSLocale *_datelocale;
+    UIDatePicker *_datePicker;
 }
 
 
-@property (strong,nonatomic)  NSString *username;
-@property (strong,nonatomic)  NSString *password;
-
-@property (strong, nonatomic) NSString *grade;
-@property (strong, nonatomic) NSString *gender;
-@property (strong, nonatomic) NSString *school;
-@property (strong, nonatomic) NSString *city;
-@property (strong, nonatomic) NSString *university;
 @property (strong,nonatomic)  UITextField *nicknamevalue;
 
-@property (strong,nonatomic) UITextField *gradevalue;
 @property (strong,nonatomic) UITextField *birdatevalue;
 @property (strong,nonatomic) UITextField *universityvalue;
 @property (strong,nonatomic) UITextField *cityvalue;
@@ -41,24 +35,13 @@
 @property (strong,nonatomic) UILabel *nickname;
 @property (strong,nonatomic) NSString *picpath;
 @property(strong, nonatomic) UISegmentedControl *genderControl;
+@property (strong, nonatomic) UIView *headerView;
+@property (strong, nonatomic) UIScrollView *scrollView;
 
 @end
 
 @implementation DDPersonalUpdateController
 
-- (id)init:(NSString *)username password:(NSString *)password city:(NSString *)city university:(NSString *)university{
-    _username=username;
-    _password=password;
-    _city=city;
-    _university=university;
-    return self;
-}
-
-- (id)init:(NSString *)nickname gender:(NSString *)gender grade:(NSString *)grade university:(NSString *)university city:(NSString *)city{
-    _nickname.text=nickname;
-   
-    return self;
-}
 
 - (void)viewDidLoad
 {
@@ -67,104 +50,118 @@
     self.title = @"修改个人资料";
     self.view.backgroundColor = [UIColor whiteColor];
     DDUser* user = [DDDataManager sharedManager].user;
+    _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:_scrollView];
+    
+    _headerView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 10, CGRectGetWidth(_scrollView.frame), 100)];
+    [_scrollView addSubview:_headerView];
     
     //学校
-    UILabel *university=[[UILabel alloc]initWithFrame:CGRectMake(self.view.frame.origin.x+10, self.view.frame.origin.y+20, 60, 30)];
+    UILabel *university=[[UILabel alloc]initWithFrame:CGRectMake(10, CGRectGetMaxY(_headerView.frame) + 20, 60, 30)];
     university.text=@"学校:";
     university.textAlignment=NSTextAlignmentLeft;
     university.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [self.view addSubview:university];
-    _universityvalue=[[UITextField alloc]initWithFrame:CGRectMake(university.frame.origin.x+50, self.view.frame.origin.y+20, 180, 30)];
+    [_scrollView addSubview:university];
+    
+    _universityvalue=[[UITextField alloc]initWithFrame:CGRectMake(university.frame.origin.x+50, CGRectGetMaxY(_headerView.frame)+20, 180, 30)];
     [_universityvalue setBorderStyle:UITextBorderStyleRoundedRect];
     _universityvalue.text= user.university;
     _universityvalue.textAlignment=NSTextAlignmentLeft;
     _universityvalue.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [self.view addSubview:_universityvalue];
+    [_scrollView addSubview:_universityvalue];
     
     //城市
     UILabel *city=[[UILabel alloc]initWithFrame:CGRectMake(10, university.frame.origin.y+40, 60, 30)];
     city.text=@"城市:";
     city.font=[UIFont fontWithName:@"Helvetica" size:12];
     city.textAlignment=NSTextAlignmentLeft;
-    [self.view addSubview:city];
+    [_scrollView addSubview:city];
     _cityvalue=[[UITextField alloc]initWithFrame:CGRectMake(city.frame.origin.x+50, university.frame.origin.y+40, 180, 30)];
     _cityvalue.text=user.city;
     [_cityvalue setBorderStyle:UITextBorderStyleRoundedRect];
     _cityvalue.textAlignment=NSTextAlignmentLeft;
     _cityvalue.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [self.view addSubview:_cityvalue];
-    
-    //年级
-//    UILabel *grade=[[UILabel alloc]initWithFrame:CGRectMake(10, city.frame.origin.y+40, 60, 30)];
-//    grade.text=@"年级:";
-//    grade.textAlignment=NSTextAlignmentLeft;
-//    grade.font=[UIFont fontWithName:@"Helvetica" size:12];
-//    [self.view addSubview:grade];
-//    _gradevalue=[[UITextField alloc]initWithFrame:CGRectMake(grade.frame.origin.x+50, city.frame.origin.y+40, 180, 30)];
-//    [_gradevalue setBorderStyle:UITextBorderStyleRoundedRect];
-//    _gradevalue.placeholder=user.grade;
-//    _gradevalue.textAlignment=NSTextAlignmentLeft;
-//    _gradevalue.font=[UIFont fontWithName:@"Helvetica" size:12];
-//    [self.view addSubview:_gradevalue];
+    [_scrollView addSubview:_cityvalue];
     
     //性别
     UILabel *gender=[[UILabel alloc]initWithFrame:CGRectMake(10, city.frame.origin.y+40, 60, 30)];
     gender.text=@"性别:";
     gender.textAlignment=NSTextAlignmentLeft;
     gender.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [self.view addSubview:gender];
+    [_scrollView addSubview:gender];
     
     NSArray* genderItems = @[@"男", @"女"];
     _genderControl = [[UISegmentedControl alloc] initWithItems:genderItems];
     _genderControl.frame = CGRectMake(gender.frame.origin.x+50, gender.frame.origin.y, 80, 25);
     _genderControl.selectedSegmentIndex = [user.gender integerValue];
-    [self.view addSubview:_genderControl];
+    [_scrollView addSubview:_genderControl];
     
     //出生日期
     UITextField *birdate=[[UITextField alloc]initWithFrame:CGRectMake(10, gender.frame.origin.y+40, 60, 30)];
     birdate.text=@"出生日期:";
     birdate.textAlignment=NSTextAlignmentLeft;
     birdate.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [self.view addSubview:birdate];
+    [_scrollView addSubview:birdate];
     _birdatevalue=[[UITextField alloc]initWithFrame:CGRectMake(gender.frame.origin.x+50, gender.frame.origin.y+40, 180, 30)];
     _birdatevalue.text=user.birthday;
     _birdatevalue.textAlignment=NSTextAlignmentLeft;
     [_birdatevalue setBorderStyle:UITextBorderStyleRoundedRect];
     _birdatevalue.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [self.view addSubview:_birdatevalue];
+    [_scrollView addSubview:_birdatevalue];
+    
+    _datePicker = [[UIDatePicker alloc] init];
+    // 時區的問題請再找其他協助 不是本篇重點
+    _datelocale = [[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"];
+    _datePicker.locale = _datelocale;
+    _datePicker.timeZone = [NSTimeZone timeZoneWithName:@"GMT"];
+    _datePicker.datePickerMode = UIDatePickerModeDate;
+    
+    // 以下這行是重點 (螢光筆畫兩行) 將 UITextField 的 inputView 設定成 UIDatePicker
+    // 則原本會跳出鍵盤的地方 就改成選日期了
+    _birdatevalue.inputView = _datePicker;
+    
+    // 建立 UIToolbar
+    UIToolbar *toolBar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 44)];
+    // 選取日期完成鈕 並給他一個 selector
+    UIBarButtonItem *right = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self
+                                                                          action:@selector(cancelDatePicker:)];
+    // 把按鈕加進 UIToolbar
+    toolBar.items = [NSArray arrayWithObject:right];
+    // 以下這行也是重點 (螢光筆畫兩行)
+    // 原本應該是鍵盤上方附帶內容的區塊 改成一個 UIToolbar 並加上完成鈕
+    _birdatevalue.inputAccessoryView = toolBar;
     
     //爱好
     UITextField *hobbies=[[UITextField alloc]initWithFrame:CGRectMake(10, birdate.frame.origin.y+40, 60, 30)];
     hobbies.text=@"爱好:";
     hobbies.textAlignment=NSTextAlignmentLeft;
     hobbies.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [self.view addSubview:hobbies];
+    [_scrollView addSubview:hobbies];
     _hobbiesvalue=[[UITextField alloc]initWithFrame:CGRectMake(birdate.frame.origin.x+50, birdate.frame.origin.y+40, 180, 30)];
     _hobbiesvalue.text=user.hobbies;
     _hobbiesvalue.textAlignment=NSTextAlignmentLeft;
     [_hobbiesvalue setBorderStyle:UITextBorderStyleRoundedRect];
     _hobbiesvalue.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [self.view addSubview:_hobbiesvalue];
+    [_scrollView addSubview:_hobbiesvalue];
     
     //签名
     UITextField *sign=[[UITextField alloc]initWithFrame:CGRectMake(10, hobbies.frame.origin.y+40, 60, 30)];
     sign.text=@"个性签名:";
     sign.textAlignment=NSTextAlignmentLeft;
     sign.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [self.view addSubview:sign];
+    [_scrollView addSubview:sign];
     _signvalue=[[UITextField alloc]initWithFrame:CGRectMake(hobbies.frame.origin.x+50, hobbies.frame.origin.y+40, 180, 30)];
     _signvalue.text=user.sign;
     _signvalue.textAlignment=NSTextAlignmentLeft;
     [_signvalue setBorderStyle:UITextBorderStyleRoundedRect];
     _signvalue.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [self.view addSubview:_signvalue];
+    [_scrollView addSubview:_signvalue];
     
     UIButton *registerButton = [[UIButton alloc] initWithFrame:CGRectMake(0, sign.frame.origin.y+40, self.view.frame.size.width, 30)];
     registerButton.backgroundColor=[UIColor redColor];
     [registerButton setTitle:@"修改" forState:UIControlStateNormal];
-    [self.view addSubview:registerButton];
+    [_scrollView addSubview:registerButton];
     [registerButton addTarget:self action:@selector(updateDDUser) forControlEvents:UIControlEventTouchUpInside];
-
 }
 
 //xiugai账号
@@ -289,12 +286,16 @@
     
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) cancelDatePicker: (id) sender {
+    if ([self.view endEditing:NO]) {
+        // 以下幾行是測試用 可以依照自己的需求增減屬性
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        NSString *dateFormat = [NSDateFormatter dateFormatFromTemplate:@"yyyy/MM/dd" options:0 locale:_datelocale];
+        [formatter setDateFormat:dateFormat];
+        [formatter setLocale:_datelocale];
+        // 將選取後的日期 填入 UITextField
+        _birdatevalue.text = [NSString stringWithFormat:@"%@",[formatter stringFromDate:_datePicker.date]];
+    }
 }
-
-
 
 @end
