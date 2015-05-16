5bb010fbe0b4f3f00e1be8feedda0bce0213ca69
liruqi <liruqi@gmail.com>
2015-05-16 21:02:22 +0800

    修改个人资料界面加照片

--- DoubleDate/Class/Settings/DDPersonalUpdateController.m
+++ DoubleDate/Class/Settings/DDPersonalUpdateController.m
@@ -14,6 +14,7 @@
 #import "AWSDynamoDB_DDUser.h"
 #import "NewSettingViewController.h"
 #import "DDDataManager.h"
+#import "UIImageView+WebCache.h"
 
 #define  PIC_WIDTH 80
 #define  PIC_HEIGHT 80
@@ -36,6 +37,8 @@
 @property (strong,nonatomic) NSString *picpath;
 @property(strong, nonatomic) UISegmentedControl *genderControl;
 @property (strong, nonatomic) UIView *headerView;
+@property (strong, nonatomic) UIView *bodyView;
+
 @property (strong, nonatomic) UIScrollView *scrollView;
 
 @end
@@ -54,60 +57,62 @@
     [self.view addSubview:_scrollView];
     
     _headerView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 10, CGRectGetWidth(_scrollView.frame), 100)];
+    _bodyView = [[UIView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(_headerView.frame) + 10, CGRectGetWidth(_scrollView.frame), CGRectGetMaxY(_scrollView.frame))];
     [_scrollView addSubview:_headerView];
-    
+    [_scrollView addSubview:_bodyView];
+
     //学校
-    UILabel *university=[[UILabel alloc]initWithFrame:CGRectMake(10, CGRectGetMaxY(_headerView.frame) + 20, 60, 30)];
+    UILabel *university=[[UILabel alloc]initWithFrame:CGRectMake(10, 10, 60, 30)];
     university.text=@"学校:";
     university.textAlignment=NSTextAlignmentLeft;
     university.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [_scrollView addSubview:university];
+    [_bodyView addSubview:university];
     
-    _universityvalue=[[UITextField alloc]initWithFrame:CGRectMake(university.frame.origin.x+50, CGRectGetMaxY(_headerView.frame)+20, 180, 30)];
+    _universityvalue=[[UITextField alloc]initWithFrame:CGRectMake(university.frame.origin.x+50, 10, 180, 30)];
     [_universityvalue setBorderStyle:UITextBorderStyleRoundedRect];
     _universityvalue.text= user.university;
     _universityvalue.textAlignment=NSTextAlignmentLeft;
     _universityvalue.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [_scrollView addSubview:_universityvalue];
+    [_bodyView addSubview:_universityvalue];
     
     //城市
     UILabel *city=[[UILabel alloc]initWithFrame:CGRectMake(10, university.frame.origin.y+40, 60, 30)];
     city.text=@"城市:";
     city.font=[UIFont fontWithName:@"Helvetica" size:12];
     city.textAlignment=NSTextAlignmentLeft;
-    [_scrollView addSubview:city];
+    [_bodyView addSubview:city];
     _cityvalue=[[UITextField alloc]initWithFrame:CGRectMake(city.frame.origin.x+50, university.frame.origin.y+40, 180, 30)];
     _cityvalue.text=user.city;
     [_cityvalue setBorderStyle:UITextBorderStyleRoundedRect];
     _cityvalue.textAlignment=NSTextAlignmentLeft;
     _cityvalue.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [_scrollView addSubview:_cityvalue];
+    [_bodyView addSubview:_cityvalue];
     
     //性别
     UILabel *gender=[[UILabel alloc]initWithFrame:CGRectMake(10, city.frame.origin.y+40, 60, 30)];
     gender.text=@"性别:";
     gender.textAlignment=NSTextAlignmentLeft;
     gender.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [_scrollView addSubview:gender];
+    [_bodyView addSubview:gender];
     
     NSArray* genderItems = @[@"男", @"女"];
     _genderControl = [[UISegmentedControl alloc] initWithItems:genderItems];
     _genderControl.frame = CGRectMake(gender.frame.origin.x+50, gender.frame.origin.y, 80, 25);
     _genderControl.selectedSegmentIndex = [user.gender integerValue];
-    [_scrollView addSubview:_genderControl];
+    [_bodyView addSubview:_genderControl];
     
     //出生日期
     UITextField *birdate=[[UITextField alloc]initWithFrame:CGRectMake(10, gender.frame.origin.y+40, 60, 30)];
     birdate.text=@"出生日期:";
     birdate.textAlignment=NSTextAlignmentLeft;
     birdate.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [_scrollView addSubview:birdate];
+    [_bodyView addSubview:birdate];
     _birdatevalue=[[UITextField alloc]initWithFrame:CGRectMake(gender.frame.origin.x+50, gender.frame.origin.y+40, 180, 30)];
     _birdatevalue.text=user.birthday;
     _birdatevalue.textAlignment=NSTextAlignmentLeft;
     [_birdatevalue setBorderStyle:UITextBorderStyleRoundedRect];
     _birdatevalue.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [_scrollView addSubview:_birdatevalue];
+    [_bodyView addSubview:_birdatevalue];
     
     _datePicker = [[UIDatePicker alloc] init];
     // 時區的問題請再找其他協助 不是本篇重點
@@ -136,32 +141,68 @@
     hobbies.text=@"爱好:";
     hobbies.textAlignment=NSTextAlignmentLeft;
     hobbies.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [_scrollView addSubview:hobbies];
+    [_bodyView addSubview:hobbies];
     _hobbiesvalue=[[UITextField alloc]initWithFrame:CGRectMake(birdate.frame.origin.x+50, birdate.frame.origin.y+40, 180, 30)];
     _hobbiesvalue.text=user.hobbies;
     _hobbiesvalue.textAlignment=NSTextAlignmentLeft;
     [_hobbiesvalue setBorderStyle:UITextBorderStyleRoundedRect];
     _hobbiesvalue.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [_scrollView addSubview:_hobbiesvalue];
+    [_bodyView addSubview:_hobbiesvalue];
     
     //签名
     UITextField *sign=[[UITextField alloc]initWithFrame:CGRectMake(10, hobbies.frame.origin.y+40, 60, 30)];
     sign.text=@"个性签名:";
     sign.textAlignment=NSTextAlignmentLeft;
     sign.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [_scrollView addSubview:sign];
+    [_bodyView addSubview:sign];
     _signvalue=[[UITextField alloc]initWithFrame:CGRectMake(hobbies.frame.origin.x+50, hobbies.frame.origin.y+40, 180, 30)];
     _signvalue.text=user.sign;
     _signvalue.textAlignment=NSTextAlignmentLeft;
     [_signvalue setBorderStyle:UITextBorderStyleRoundedRect];
     _signvalue.font=[UIFont fontWithName:@"Helvetica" size:12];
-    [_scrollView addSubview:_signvalue];
+    [_bodyView addSubview:_signvalue];
     
     UIButton *registerButton = [[UIButton alloc] initWithFrame:CGRectMake(0, sign.frame.origin.y+40, self.view.frame.size.width, 30)];
     registerButton.backgroundColor=[UIColor redColor];
     [registerButton setTitle:@"修改" forState:UIControlStateNormal];
-    [_scrollView addSubview:registerButton];
+    [_bodyView addSubview:registerButton];
     [registerButton addTarget:self action:@selector(updateDDUser) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    // reload images
+    DDUser* user = [DDDataManager sharedManager].user;
+    NSCharacterSet *cs = [NSCharacterSet characterSetWithCharactersInString: @","];
+    NSString *photo = [user.photos stringByTrimmingCharactersInSet:cs];
+    NSArray *photoArray = [photo componentsSeparatedByString:@","];
+    if (photoArray.count> 0) {
+        int i = 0;
+        for (id element in photoArray) {
+            if (element != nil && ![element isEqual:@""]) {
+                //图片显示
+                UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(PIC_WIDTH * i, 0, PIC_WIDTH, PIC_HEIGHT)];
+                
+                NSString* url = DD_PHOTO_URL(user.UID, element);
+                [imageView sd_setImageWithURL:[NSURL URLWithString:url] placeholderImage:[UIImage imageNamed:@"Logo_new"]];
+                
+                //获取图片的框架，得到长、宽
+                //赋值
+                imageView.tag = i;
+                //ScrollView添加子视图
+                [_headerView addSubview:imageView];
+                i++;
+            }
+        }
+        //_headerView.frame = cell.contentView.bounds;
+        _headerView.frame = CGRectMake(0, 10, CGRectGetWidth(_scrollView.frame), 100);
+
+    } else {
+        _headerView.frame = CGRectZero;
+    }
+    
+    _bodyView.frame = CGRectMake(0, CGRectGetMaxY(_headerView.frame) + 10, CGRectGetWidth(_scrollView.frame), CGRectGetMaxY(_scrollView.frame));
 }
 
 //xiugai账号
